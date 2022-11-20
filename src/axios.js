6cc1a2e145f019cd43d7665f5ce7d476ec29d9d8
duyth <duyth@tokyotechlab.com>
2022-11-20 13:50:18 +0700

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_CLIENT_URL,
+  // baseURL: process.env.REACT_APP_CLIENT_URL,
+  baseURL: process.env.REACT_SEA_FOOD_URL,
 });
